4c58721e207490a6d4dbc089be2aef35bac6cda0
Ragavendran Mrn <36659683+RagavendranMRN@users.noreply.github.com>
2020-05-13 08:27:09 +0530

    Create MorseCode.py

---
+++ MorseCode.py
@@ -0,0 +1,33 @@
+import winsound
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 
+                    'C':'-.-.', 'D':'-..', 'E':'.', 
+                    'F':'..-.', 'G':'--.', 'H':'....', 
+                    'I':'..', 'J':'.---', 'K':'-.-', 
+                    'L':'.-..', 'M':'--', 'N':'-.', 
+                    'O':'---', 'P':'.--.', 'Q':'--.-', 
+                    'R':'.-.', 'S':'...', 'T':'-', 
+                    'U':'..-', 'V':'...-', 'W':'.--', 
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+                    '1':'.----', '2':'..---', '3':'...--', 
+                    '4':'....-', '5':'.....', '6':'-....', 
+                    '7':'--...', '8':'---..', '9':'----.', 
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-', 
+                    '?':'..--..', '/':'-..-.', '-':'-....-', 
+                    '(':'-.--.', ')':'-.--.-',' ':' | '} 
+
+Morsecoded = ''
+Message = input('Enter your message: ').upper()
+
+for char in Message:
+    if char in MORSE_CODE_DICT.keys():
+        Morsecoded += MORSE_CODE_DICT[char] + ' '
+        for code in MORSE_CODE_DICT[char]:
+        	if code == ".":
+        		winsound.Beep(2000, 75)
+        	elif code == "-":
+        		winsound.Beep(2000, 200)
+
+        winsound.Beep(40, 200)
+
+print(f'Orginal: {Message}\nCoded: {Morsecoded}')
