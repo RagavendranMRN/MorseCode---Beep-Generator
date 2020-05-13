6dd3188c4e2e93549dd7c0b3c5bf2fedfb1e9ace
Ragavendran Mrn <36659683+RagavendranMRN@users.noreply.github.com>
2020-05-13 11:53:41 +0530

    Update MorseCode.py

--- MorseCode.py
+++ MorseCode.py
@@ -1,4 +1,5 @@
 import winsound
+import time
 
 MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 
                     'C':'-.-.', 'D':'-..', 'E':'.', 
@@ -28,6 +29,6 @@ for char in Message:
         	elif code == "-":
         		winsound.Beep(2000, 200)
 
-        winsound.Beep(40, 200)
+        time.sleep(0.5)
 
 print(f'Orginal: {Message}\nCoded: {Morsecoded}')
